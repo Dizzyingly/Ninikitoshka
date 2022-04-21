0c2750da7b035323aaefc200a503c8900b1bb477
gosha5063 <goshavlasov2005@gmail.com>
2022-04-22 00:02:08 +0300

    new f

---
+++ converter.py
@@ -0,0 +1,11 @@
+import pickle
+
+
+class PeopleTel:
+    def __init__(self, numberroom, lastName, firstName, patronymic, telephone):
+        self.numberroom = numberroom
+        self.firstName = firstName
+        self.lastName = lastName
+        self.patronymic = patronymic
+        self.telephone = telephone
+
